b8afe52027402520f66c612a4db45014bb2ac45f
Barkisjr <armandas0707@gmail.com>
2020-12-19 19:27:22 +0200

    added filter

--- resources/views/product/products.blade.php
+++ resources/views/product/products.blade.php
@@ -5,13 +5,36 @@
 
     <div class="card-body">
         <div class="container" style="margin:20px">
-        
-            
-            
-                
-                
+        @if(isset($array2))
+            <input readonly type="text" id="by" name="by" value="{{$array2['by']}}">
+            <input readonly type="text" id="value" name="value" value=" {{ $array2['value'] }}"><br><br>
+            <input readonly type="text" id="by" name="by" value="{{$array2['by2']}}">
+            <input readonly type="text" id="value" name="value" value=" {{ $array2['value2'] }}"><br><br>
+        @elseif(isset($array))
+           
+            <form method="get" action="{{route('viewFilteredProductsAfterTwoFilters')}}">
+                <input readonly type="text" id="by" name="by" value="{{$array['by']}}"> 
+                <input readonly type="text" id="value" name="value" value=" {{ $array['value'] }}"><br><br>
+
+
+                <select name="by2" id="by2">
+                    @if($array['by'] == 'name')
+                        <option value="price">Price</option>
+                        <option value="category">Category</option>
+                    @elseif($array['by'] == 'price')
+                        <option value="name">Name</option>
+                        <option value="category">Category</option>
+                    @else
+                        <option value="name">Name</option>
+                        <option value="price">Price</option>
+                    @endif 
+                </select>
+                <input type="text" id="value2" name="value2" value=""><br>
+                <button type="submit" class="btn btn-danger">Filter</button>
+            </form>
+        @else
             <form method="get" action="{{route('viewFilteredProducts') }}">
-            <select name="by" id="by">
+                <select name="by" id="by">
                     <option value="name">Name</option>
                     <option value="price">Price</option>
                     <option value="category">Category</option>
@@ -19,6 +42,10 @@
                 <input type="text" id="value" name="value" value=""><br>
                 <button type="submit" class="btn btn-danger">Filter</button>
             </form>
+        @endisset
+
+
+
 
             <hr>
             <table class="table table-bordered">
@@ -39,8 +66,6 @@
                         <td>{{$product->price}}</td>
                         <td>{{$product->category }}</td>
                         <td>
-
-
                             <form style="display: inline;" method="get" action="{{ route('viewSingleProduct', $product->id ) }}">
 
                                 <button type="submit" class="btn btn-danger">View product</button>
